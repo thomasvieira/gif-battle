faed8d408da1681c8b509ab80915762266d38899
THOMAS <thomas@rocketlab.com.br>
2020-06-15 13:08:14 -0300

    commit initial gif-battle

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+} from "react-router-dom";
+
+import Home from "./Home";
+import GifDetails from "./GifDetails";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="flex bg-green-500 text-white p-5">
+        <div className="w-1/12">Mongo Gif-Battle</div>
+        <div className="w-11/12 font-bold">
+          <Link to="/">Home</Link>
+        </div>
+      </div>
+      <iframe
+        width="640"
+        height="480"
+        src="https://charts.mongodb.com/charts-project-0-kmqgv/embed/charts?id=d550cbd5-ebd0-4846-a383-3a11a74cae4c&autorefresh=10&theme=light"
+      >        
+      </iframe>
+      <Switch>
+        <Route path="/gif/:id">
+          <GifDetails />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
